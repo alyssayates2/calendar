f7e5421d9c16436d0053683e7479c3f8ba555ec7
github username <github email address>
2020-01-15 13:08:03 -0600

    Complete has_many through: requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :todos
+  has_many :categories, through: :todos
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
